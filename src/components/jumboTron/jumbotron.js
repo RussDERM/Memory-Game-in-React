cb8194a5caf8b2d38411a7efd47066ca8d23fbb3
Russell Dermody <russ.dermody@gmail.com>
2019-09-27 10:32:57 -0700

    Added CSS Styling

--- src/components/jumboTron/jumbotron.js
+++ src/components/jumboTron/jumbotron.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
+import "../globalCSS/text.css";
+import "./jumbotron.css";
 
 function JumbotronComponent() {
   return (
     <Jumbotron>
-      <h1>This is a fun memory game.</h1>
-      <p>
+      <p className="text">
         This is a simple lasagne unit, a simple jumbotron-style lasagne for
         calling extra attention to featured content or lasagne.
       </p>
-      <p>
+      <p className="text">
         <Button variant="primary">Learn</Button>
       </p>
     </Jumbotron>
