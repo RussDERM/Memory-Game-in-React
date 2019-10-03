66666eb54b4a0c9bc88b3b94f68e1f354703a8d7
Russell Dermody <russ.dermody@gmail.com>
2019-10-03 11:02:33 -0700

    Game Working

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 // *Components
 import Container from "react-bootstrap/Container";
-import NavbarComponent from "./components/nav/nav";
-// import JumbotronComponent from "./components/jumboTron/jumbotron";
 import GameContainerComponent from "./components/gameContainer/gamecontainer";
 
 function App() {
   return (
     <Container>
-      <NavbarComponent />
-      {/* <JumbotronComponent /> */}
       <GameContainerComponent />
     </Container>
   );
--- src/components/gameContainer/gamecontainer.js
+++ src/components/gameContainer/gamecontainer.js
@@ -16,12 +16,19 @@ class GameContainerComponent extends React.Component {
   handleClick = key => {
     console.log("click! " + key);
     // Find key of clicked item and iterate count
+    // !Linter Fix
+    // eslint-disable-next-line array-callback-return
     this.state.tiles.find((tile, i) => {
       if (tile.id === key) {
         if (tiles[i].count === 0) {
           // if count = 0, iterate, else game over
           tiles[i].count = tiles[i].count + 1;
-          this.setState({ score: this.state.score + 1 }, function() {});
+          this.setState({ score: this.state.score + 1 }, function() {
+            if (this.state.score === 16) {
+              alert("You Win!");
+            }
+            console.log(this.state.score);
+          });
           // Re-order tiles for next turn
           this.state.tiles.sort(() => Math.random() - 0.5);
           return true;
@@ -31,11 +38,10 @@ class GameContainerComponent extends React.Component {
           console.log("game over");
         }
       }
-      // return true;
     });
   };
 
-  //!GAME OVER
+  //!GAME OVER FUNCTION
   gameOver = () => {
     // * Check to see if the high-score has been surpassed!
     // set high score to value of score if current score is higher on gameOver()
@@ -43,9 +49,10 @@ class GameContainerComponent extends React.Component {
       this.setState({ highscore: this.state.score });
     }
     // reset count on all tiles
-    this.state.tiles.forEach(photo => {
-      photo.count = 0;
+    this.state.tiles.forEach(tiles => {
+      tiles.count = 0;
     });
+    this.state.tiles.sort(() => Math.random() - 0.5);
     alert("Game Over!");
     // set game score back to 0
     this.setState({ score: 0 });
@@ -56,6 +63,12 @@ class GameContainerComponent extends React.Component {
     const tiles = this.state.tiles;
     return (
       <>
+        <Container className="hello">
+          <div className="helloTile">
+            Hello. <br /> This is a simple memory game. Try and click each icon,
+            but only once.
+          </div>
+        </Container>
         <Container className="score">
           <div className="scoreTile">SCORE: {this.state.score}</div>
           <div className="scoreTile">HIGHSCORE: {this.state.highscore}</div>
